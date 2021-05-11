0b6a4d2695d185b4477e53fb910c9c0836e3c273
touddld <565935215@qq.com>
2021-05-11 20:57:57 +0800

    2021.5.11

---
+++ FileSearch/NewTestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSearch
+{
+    //这个类文件仅仅是用来测试
+    class NewTestClass
+    {
+        public NewTestClass()
+        {
+            var b = "hellow,world";
+        }
+    }
+}
